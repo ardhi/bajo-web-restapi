62da895b44ae711ebbe3eafbb1c381995e8b2132
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-16 17:50:56 +0700

    Updates

--- bajo/helper/doc-schema/coll.js
+++ bajo/helper/doc-schema/coll.js
@@ -8,11 +8,12 @@ function getType (input) {
   return type
 }
 
-async function buildPropsReqs (schema, method, hidden = []) {
+async function buildPropsReqs ({ schema, method, options = {} }) {
   const { getSchema } = this.bajoDb.helper
   const properties = {}
   const required = []
   const rels = {}
+  const hidden = options.hidden ?? []
   for (const p of schema.properties) {
     if (hidden.includes(p.name)) continue
     properties[p.name] = { type: getType(p.type) }
@@ -43,19 +44,18 @@ async function buildPropsReqs (schema, method, hidden = []) {
   return { properties, required }
 }
 
-async function buildResponse (ctx, schema, method, hidden) {
+async function buildResponse ({ ctx, schema, method, options }) {
   const { print, getConfig, importPkg } = this.bajo.helper
   const { transformResult, docSchemaLib, docSchemaForFind } = this.bajoWebRestapi.helper
   const { merge, cloneDeep } = await importPkg('lodash-es')
   const cfgWeb = getConfig('bajoWeb')
-  const { properties } = await buildPropsReqs.call(this, schema, method, hidden)
+  const { properties } = await buildPropsReqs.call(this, { schema, method, options })
 
-  async function buildData (keys, noRels) {
+  async function buildData (keys) {
     const data = {}
     for (const k of keys) {
       const name = 'coll' + schema.name
       const props = cloneDeep(properties)
-      if (noRels) delete props._rel
       await docSchemaLib(ctx, name, {
         type: 'object',
         properties: props
@@ -92,16 +92,16 @@ async function buildResponse (ctx, schema, method, hidden) {
     return result
   }
   const success = { type: 'boolean', default: true }
-  let statusCode = { type: 'integer', default: 200 }
+  const statusCode = { type: 'integer', default: 200 }
   if (method === 'get') {
     result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['data']), { success, statusCode }) })
   } else if (method === 'create') {
-    statusCode = 201
-    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['data'], true), { success, statusCode }) })
+    statusCode.default = 201
+    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['data']), { success, statusCode }) })
   } else if (['update', 'replace'].includes(method)) {
-    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['data', 'oldData'], true), { success, statusCode }) })
+    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['data', 'oldData']), { success, statusCode }) })
   } else if (method === 'remove') {
-    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['oldData'], true), { success, statusCode }) })
+    result['2xx'].properties = await transformResult({ data: merge({}, await buildData.call(this, ['oldData']), { success, statusCode }) })
   } else if (method === 'find') {
     result['2xx'].properties = await transformResult({
       data: await docSchemaForFind(ctx, { type: 'object', properties }),
@@ -130,8 +130,9 @@ async function docSchemaColl ({ coll, method, ctx, options = {} }) {
     if (!options.noId) out.params = { $ref: 'paramsId#' }
   }
   if (['update'].includes(method)) {
-    const { properties } = await buildPropsReqs.call(this, schema, method, options.hidden)
+    const { properties } = await buildPropsReqs.call(this, { schema, method, options })
     const name = 'coll' + schema.name + 'Update'
+    delete properties._rel
     await docSchemaLib(ctx, name, {
       type: 'object',
       properties: omit(properties, ['id'])
@@ -139,8 +140,9 @@ async function docSchemaColl ({ coll, method, ctx, options = {} }) {
     out.body = { $ref: name + '#' }
   }
   if (['replace'].includes(method)) {
-    const { properties, required } = await buildPropsReqs.call(this, schema, method, options.hidden)
+    const { properties, required } = await buildPropsReqs.call(this, { schema, method, options })
     const name = 'coll' + schema.name + 'Replace'
+    delete properties._rel
     await docSchemaLib(ctx, name, {
       type: 'object',
       properties: omit(properties, ['id']),
@@ -149,8 +151,9 @@ async function docSchemaColl ({ coll, method, ctx, options = {} }) {
     out.body = { $ref: name + '#' }
   }
   if (['create'].includes(method)) {
-    const { properties, required } = await buildPropsReqs.call(this, schema, method, options.hidden)
+    const { properties, required } = await buildPropsReqs.call(this, { schema, method, options })
     const name = 'coll' + schema.name + 'Create'
+    delete properties._rel
     await docSchemaLib(ctx, name, {
       type: 'object',
       properties,
@@ -159,7 +162,7 @@ async function docSchemaColl ({ coll, method, ctx, options = {} }) {
     out.body = { $ref: name + '#' }
     out.querystring = { $ref: 'qsFields#' }
   }
-  out.response = await buildResponse.call(this, ctx, schema, method, options.hidden)
+  out.response = await buildResponse.call(this, { ctx, schema, method, options })
   return out
 }
 
